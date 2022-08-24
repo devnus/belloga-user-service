ca9ae70062f50f21df73b86b72220cd246b65acb
genius-jo <joks7285@daum.net>
2022-08-24 14:57:20 +0900

    fix: Fix Kafka consumer config

--- src/main/java/com/devnus/belloga/user/common/config/KafkaConsumerConfig.java
+++ src/main/java/com/devnus/belloga/user/common/config/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.devnus.belloga.user.common.config;
 
+import com.devnus.belloga.user.user.dto.EventAccount;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,15 +24,19 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.enable-auto-commit}")
     private boolean AUTO_COMMIT;
 
-    @Bean
-    ConsumerFactory<String,Object> consumerFactory(){
+    private Map<String, Object> configProps() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class); //카프카에서 받은 키 역 직렬화
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class); //카프카에서 받은 값 역 직렬화
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, AUTO_OFFSET_RESET);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, AUTO_COMMIT);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(Object.class));
+        return props;
+    }
+
+    @Bean
+    ConsumerFactory<String,Object> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(configProps(), new StringDeserializer(), new JsonDeserializer<>(Object.class));
     }
 
     @Bean
@@ -40,5 +45,19 @@ public class KafkaConsumerConfig {
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
+
+    @Bean
+    ConsumerFactory<String, EventAccount.RegisterAccountEnterprise> eventAccountRegisterAccountEnterpriseFactory(){
+        return new DefaultKafkaConsumerFactory<>(configProps(), new StringDeserializer(), new JsonDeserializer<>(EventAccount.RegisterAccountEnterprise.class));
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, EventAccount.RegisterAccountEnterprise> eventAccountRegisterAccountEnterpriseListener(){
+        ConcurrentKafkaListenerContainerFactory<String, EventAccount.RegisterAccountEnterprise> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(eventAccountRegisterAccountEnterpriseFactory());
+        return factory;
+    }
+
+
 
 }
--- src/main/java/com/devnus/belloga/user/user/event/UserConsumer.java
+++ src/main/java/com/devnus/belloga/user/user/event/UserConsumer.java
@@ -17,8 +17,8 @@ public class UserConsumer {
      * 기업 사용자 custom 회원가입시 consume
      * group은 협의 후 결정
      */
-    @KafkaListener(topics = "register-account-enterprise", groupId = "register-account-enterprise-1")
-    protected boolean registerCustomAccountEnterprise(Object event) throws IOException {
-        return userService.saveUserEnterprise((EventAccount.RegisterAccountEnterprise) event);
+    @KafkaListener(topics = "register-account-enterprise", groupId = "register-account-enterprise-1", containerFactory = "eventAccountRegisterAccountEnterpriseListener")
+    protected boolean registerCustomAccountEnterprise(EventAccount.RegisterAccountEnterprise event) throws IOException {
+        return userService.saveUserEnterprise(event);
     }
 }
